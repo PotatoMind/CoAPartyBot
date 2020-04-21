1f12588752c0aeb53e50fafa0dbfad439b2a7897
Aaron <hammer.21@wright.edu>
2020-04-21 18:55:41 -0400

    wiki search!

---
+++ cogs/wiki.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands, tasks
+import json
+import requests
+from urllib.parse import quote 
+
+class Wiki(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.command()
+    async def wiki_search(self, ctx, *, search_term):
+        req = requests.get(f'https://www.curseofaros.wiki/api.php?action=opensearch&search={quote(search_term)}&format=json')
+        json_data = json.loads(req.text)
+        if 'error' in json_data:
+            await ctx.send('Search has broke. Contact the dev.')
+        elif len(json_data[3]) == 0:
+            await ctx.send('Could not find that search term.')
+        else:
+            embed = discord.Embed(
+                title=f'Search for {search_term}',
+                description='\n'.join([link for link in json_data[3]]),
+                color=discord.Color.magenta(),
+            )
+            await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Wiki(client))
+    print('Loaded Wiki')
