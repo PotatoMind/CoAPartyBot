14fa50490567d0a05e787a7a90f9e54d14d5e342
Aaron <aaronh2015@hotmail.com>
2021-01-04 15:43:01 -0500

    removed automatic loading of prefixes into db

--- bot.py
+++ bot.py
@@ -20,17 +20,6 @@ def get_prefix(bot, message):
         bot.db.prefixes.insert_one(prefix_info)
         return '!'
 
-def json_to_db(collection):
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-    for guild_id, info in config.items():
-        prefix_info = {
-            'guild_id': guild_id,
-            'prefix': info['prefix']
-        }
-        collection.insert_one(prefix_info)
-        print(f'inserted {prefix_info}')
-
 bot = commands.Bot(command_prefix=get_prefix)
 bot.launch_time = datetime.utcnow()
 
@@ -45,7 +34,6 @@ token = settings['token']
 bot.owner_id = settings['owner_id']
 mongo_client = MongoClient(settings['mongo_uri'])
 bot.db = mongo_client['coa']
-json_to_db(bot.db.prefixes)
 player_cache = Redis.from_url(settings['redis_url'], db=0)
 max_page_cache = Redis.from_url(settings['redis_url'], db=1)
 bot.player_cache = player_cache
