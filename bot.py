dcfd1bce971fd529659909d9e0840d3674e7a554
Aaron <aaronh2015@hotmail.com>
2021-01-04 15:41:04 -0500

    prefix loading from mongo instead of json

--- bot.py
+++ bot.py
@@ -8,16 +8,28 @@ from pymongo.collation import Collation
 from redis.client import Redis
 
 def get_prefix(bot, message):
+    guild_id = str(message.guild.id)
+    prefix_info = bot.db.prefixes.find_one({'guild_id': guild_id})
+    if prefix_info:
+        return prefix_info['prefix']
+    else:
+        prefix_info = {
+            'guild_id': guild_id,
+            'prefix': '!'
+        }
+        bot.db.prefixes.insert_one(prefix_info)
+        return '!'
+
+def json_to_db(collection):
     with open('config.json', 'r') as f:
         config = json.load(f)
-    try:
-        prefix = config[str(message.guild.id)]['prefix']
-    except:
-        config[str(message.guild.id)] = {'prefix': '!'}
-        with open('config.json', 'w') as f:
-            json.dump(config, f)
-        prefix = '!'
-    return prefix
+    for guild_id, info in config.items():
+        prefix_info = {
+            'guild_id': guild_id,
+            'prefix': info['prefix']
+        }
+        collection.insert_one(prefix_info)
+        print(f'inserted {prefix_info}')
 
 bot = commands.Bot(command_prefix=get_prefix)
 bot.launch_time = datetime.utcnow()
@@ -33,6 +45,7 @@ token = settings['token']
 bot.owner_id = settings['owner_id']
 mongo_client = MongoClient(settings['mongo_uri'])
 bot.db = mongo_client['coa']
+json_to_db(bot.db.prefixes)
 player_cache = Redis.from_url(settings['redis_url'], db=0)
 max_page_cache = Redis.from_url(settings['redis_url'], db=1)
 bot.player_cache = player_cache
